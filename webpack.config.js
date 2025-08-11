fb422d04534b177bc4d4af168d9033aa84123145
Pucho <martinezochandarenam@gmail.com>
2025-08-11 21:27:35 +0200

    feat: minificacion con mode production and hashin main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path'); // me crea una ruta en al que al uinicio rtrienen la direccion en la esta el archivo
 
-module.exports = {
-	//entry: "", // le decimos el punto de entrada, lo saba reconocer por defecto "index.js"
+module.exports = (env, args ) => {
+	const { mode } = args;
+
+	const isProducction  = mode === 'production'
+
+	return {
+		//entry: "", // le decimos el punto de entrada, lo saba reconocer por defecto "index.js"
 	output: {
+		filename: isProducction ? '[name].[contenthash].js' : 'main.js', // hashear la salida 
 		path: path.resolve(__dirname,  'build' ) // este seria la ruta en la que webpack generaria el js compilado para navegadores
 	},
 	// caracteristicas agregadas a webpack
@@ -42,4 +48,6 @@ module.exports = {
 	/* devtool: 'source-map' --> ayuda a depurar el codigo creando dos archivos  
 	   en el cual uno es exactamente la copi del codigo puedes encontrar los errores exactos por linea 
 	    es sumamente costoso */
+	}
+	
 }
